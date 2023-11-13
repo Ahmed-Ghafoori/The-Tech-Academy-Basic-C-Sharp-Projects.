6ab77d22b00711ab42851da4c55af292d3783251
Ahmed-Ghafoori <136653227+Ahmed-Ghafoori@users.noreply.github.com>
2023-11-14 00:42:55 +0400

    Add files via upload

---
+++ Polymorphism-Assignment.cs
@@ -0,0 +1,31 @@
+﻿// Create the IQuittable interface
+public interface IQuittable
+{
+    // Define the Quit method
+    void Quit();
+}
+
+// Employee class from the previous drill
+public class Employee
+{
+    // ... (other code)
+
+    // Implement the IQuittable interface and define the Quit method
+    public void Quit()
+    {
+        Console.WriteLine("Employee is quitting!");
+    }
+}
+
+// Main program
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create an object of type IQuittable (which is Employee)
+        IQuittable quittableEmployee = new Employee();
+
+        // Call the Quit method on the object using polymorphism
+        quittableEmployee.Quit();
+    }
+}
